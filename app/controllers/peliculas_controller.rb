214fe2fc73a153ec8e893f75a581ddc2c50570bf
David <davidgarrofe@gmail.com>
2017-05-25 16:52:22 +0200

    pelicula 1

---
+++ app/controllers/peliculas_controller.rb
@@ -0,0 +1,61 @@
+class PeliculasController < ApplicationController
+
+	def index
+		@peliculas = Pelicula.all
+   	end
+   
+	def show
+		@pelicula = Pelicula.find(params[:id])
+		if @pelicula.name == 'Thor'
+			@nom_de_peli = 1
+		elsif @pelicula.name == 'cenicienta'
+			@nom_de_peli = 2
+		else
+			@nom_de_peli = 3
+		end
+	end
+	   
+	def new
+		@pelicula = Pelicula.new
+	end
+	   
+	def create
+		@pelicula = Pelicula.new(name: params[:pelicula][:name],
+								stars: params[:pelicula][:stars],
+								main_actor: params[:pelicula][:main_actor],
+								year: params[:pelicula][:year])
+		if @pelicula.save
+			redirect_to peliculas_path, :notice => "Se ha guardado la peli!"
+		else
+			render 'new'	
+		end
+	end
+	   
+	def edit
+		@pelicula = Pelicula.find(params[:id])
+	end
+	   
+	def update
+		@pelicula = Pelicula.find(params[:id])
+ 
+		if @pelicula.update_attributes(name: params[:pelicula][:name],
+								stars: params[:pelicula][:stars],
+								main_actor: params[:pelicula][:main_actor],
+								year: params[:pelicula][:year])
+
+			redirect_to peliculas_path, :notice => 'éxito!'
+		else
+			render 'edit'
+		end
+
+	end
+	   
+	def destroy
+		@pelicula = Pelicula.find(params[:id]).destroy
+    	# @pelicula.destroy
+    	redirect_to peliculas_path, :notice => "La teva pelicula ha sigut eliminada"
+
+    	#render 'index'
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'bienvenido/index'
   resources :articulos
   root 'bienvenido#index'
+  	get 'users/index'
+	get 'users/current_user'
+	get 'peliculas/index'
+	get 'peliculas/show'
+	resources :users
+	resources :peliculas
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
