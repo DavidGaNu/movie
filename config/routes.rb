d1603fff668ce66fe1db7cf6bc2a9b2fcc86a46d
David <davidgarrofe@gmail.com>
2017-06-08 13:26:59 +0200

    01-06 v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'settings/index'
+  # devise_for :admin_users, ActiveAdmin::Devise.config
   # resources :pelis
-  devise_for :users
+  # devise_for :users
   get 'bienvenido/index'
   # resources :articulos
   # get 'bienvenido/index'
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
 	  get 'peliculas/show'
 	resources :users
 	resources :peliculas
+  resources :settings
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
